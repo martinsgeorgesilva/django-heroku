87d02e57fea78d7b00c779ef07622a5f12df25fa
georgemartinsdasilva <gmartins86@yahoo.com.br>
2021-03-25 23:44:06 -0300

    alterações saulo segunda rodada

--- django_heroku/settings.py
+++ django_heroku/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'django_heroku.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+'''
 
 
 DATABASES = {
@@ -92,7 +93,7 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3")
 }
-'''
+
 
 
 # Password validation
